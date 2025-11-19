2c6adf86233627dad5b31380073a2ba249401a7b
cheryl1214 <cheryl.xie@dawsoncollege.qc.ca>
2025-11-19 14:24:00 -0500

    Update bme290Readings.php

--- php/bme290Readings.php
+++ php/bme290Readings.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['button'])){
-    $raw = `./bme280`;
+    $raw = `./var/www/html/webserverfunds/raspberry-pi-bme280/bme280`;
     echo $raw;
     $deserialized = json_decode($raw, true);
     var_dump($deserialized);
