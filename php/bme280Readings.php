8f35a84f1c4016a6d2549f9b1f93e3b795f7c954
cheryl1214 <cheryl.xie@dawsoncollege.qc.ca>
2025-11-19 14:42:49 -0500

    Update bme280Readings.php

--- php/bme280Readings.php
+++ php/bme280Readings.php
@@ -1,7 +1,6 @@
 <?php
 if(isset($_POST['button'])){
     $raw = `cd /var/www/html/webserverfunds/raspberry-pi-bme280/ && ./bme280`;
-    echo $raw;
     $deserialized = json_decode($raw, true);
     var_dump($deserialized);
 }
@@ -10,15 +9,16 @@ if(isset($_POST['button'])){
 <html>
     <head>
         <title>BME280 Readings</title>
+        <style>
+            h2{ margin: 0px; margin-left: 8px;}
+            body {
+            font-family: "Century Gothic";
+        }
+        </style>
     </head>
     <body>
         <h2>BME280 Sensor Readings</h2>
-        <div style="color: white;
-            background: rgb(66, 139, 174); 
-            border-radius: 10px;
-            padding: 10px; 
-            font-family: 'Courier New', Courier, monospace;
-            font-size: 1em; ">
+        <div style="font-family: 'Century Gothic'; color: white; border-radius: 10px; padding: 20px;">
         Sensor: <?php echo htmlspecialchars($deserialized["sensor"]); ?>
         Humidity: <?php echo htmlspecialchars($deserialized["humidity"]); ?>
         Pressure: <?php echo htmlspecialchars($deserialized["pressure"]); ?>
