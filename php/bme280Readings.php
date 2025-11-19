aca2066568fe4b875494c6a3d5df90723044e2ff
cheryl1214 <cheryl.xie@dawsoncollege.qc.ca>
2025-11-19 14:39:42 -0500

    Update bme280Readings.php

--- php/bme280Readings.php
+++ php/bme280Readings.php
@@ -4,6 +4,27 @@ if(isset($_POST['button'])){
     echo $raw;
     $deserialized = json_decode($raw, true);
     var_dump($deserialized);
-    echo htmlspecialchars($deserialized);
 }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BME280 Readings</title>
+    </head>
+    <body>
+        <h2>BME280 Sensor Readings</h2>
+        <div style="color: white;
+            background: rgb(66, 139, 174); 
+            border-radius: 10px;
+            padding: 10px; 
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 1em; ">
+        Sensor: <?php echo htmlspecialchars($deserialized["sensor"]); ?>
+        Humidity: <?php echo htmlspecialchars($deserialized["humidity"]); ?>
+        Pressure: <?php echo htmlspecialchars($deserialized["pressure"]); ?>
+        Temperature: <?php echo htmlspecialchars($deserialized["temperature"]); ?>
+        Altitude: <?php echo htmlspecialchars($deserialized["altitude"]); ?>
+        Timestamp: <?php echo htmlspecialchars($deserialized["timestamp"]); ?>
+        </div>
+    </body>
+</html>
