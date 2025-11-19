8d920288b7a6b355b796ca3f752ec72487ac6db8
cheryl1214 <cheryl.xie@dawsoncollege.qc.ca>
2025-11-19 13:45:22 -0500

    Update activateLED.php

--- php/activateLED.php
+++ php/activateLED.php
@@ -3,6 +3,6 @@ if(isset($_POST['button'])){
     $toggle = shell_exec('gpio toggle 0');
     echo $toggle;
     $read = shell_exec('gpio read 0');
-    echo $read;
+    echo htmlspecialchars($read);
 }
 ?>
